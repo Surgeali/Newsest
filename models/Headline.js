79923ee6785f3d8a993e828584534566349b28cd
Surge Ali <surgeali@gmail.com>
2019-08-08 16:55:21 -0700

    initial connect

--- models/Headline.js
+++ models/Headline.js
@@ -8,17 +8,37 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 // Create the headlineSchema with our schema class
-var headlineSchema = new Schema({
-  // headline, a string, must be entered
-
-  // summary, a string, must be entered
-
-  // url, a string, must be entered
-
-  // date is just a string
-
-  // saved, boolean default false
-});
+  var headlineSchema = new Schema({
+    // headline, a string, must be entered
+    string: {
+      type: String,
+      trim: true,
+      required: true
+    },
+    // summary, a string, must be entered
+    string: {
+      type: String,
+      trim: true,
+      required: true
+    },
+    // url, a string, must be entered
+    string: {
+      type: String,
+      trim: true,
+      required: true
+    },
+    // date is just a string
+    date: {
+      type: Date,
+      default: Date.now
+    },
+    // saved, boolean default false
+    boolean: {
+      type: Boolean,
+      default: false
+    }
+  
+  });
 
 // Create the Headline model using the headlineSchema
 var Headline = mongoose.model("Headline", headlineSchema);
